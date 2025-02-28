f911b5f8fa9c2f7878b57463ea9e05004859879c
David-A8 <davidamadorc@gmail.com>
2025-02-28 15:24:59 -0700

    Changes for NPS2 were made

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,36 @@
 import { getParkData } from "./parkService.mjs";
-
 const parkData = getParkData();
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+setHeaderInfo(parkData);
+
+const parkInfoLinks = [
+    {
+        name: "Current Conditions &#x203A;",
+        link: "conditions.html",
+        image: parkData.images[2].url,
+        description: "See what conditions to expect in the park before leaving on your trip!"
+    },
+    {
+        name: "Fees and Passes &#x203A;",
+        link: "fees.html",
+        image: parkData.images[3].url,
+        description: "Learn about the fees and passes that are available."
+    },
+    {
+        name: "Visitor Centers &#x203A;",
+        link: "visitor_centers.html",
+        image: parkData.images[9].url,
+        description: "Learn about the visito centers in the park."
+    }
+]
+
+function setHeaderInfo(data) {
+    const disclaimer = document.querySelector(".disclaimer > a");
+    disclaimer.href = parkData.url;
+    disclaimer.innerHTML = parkData.fullName;
+    document.querySelector(".hero-banner > img").src = parkData.images[0].url;
+    document.querySelector("head > title").textContent = parkData.fullName;
+    document.querySelector(".hero-banner_content").innerHTML = parkInfoTemplate(parkData);
+}
 
 function parkInfoTemplate(info) {
     return `<a href="/" class="hero-banner_title">${info.name}</a>
@@ -13,8 +40,6 @@ function parkInfoTemplate(info) {
     </p>`;
 }
 
-document.querySelector(".hero-banner >img").src = parkData.images[0].url;
+function mediaCardTemplate(info) {
 
-document.querySelector("head > title").textContent = parkData.fullName;
-
-document.querySelector(".hero-banner_content").innerHTML = parkInfoTemplate(parkData);
+}
